5b165191ccd58a1d6f11adeb62dea32127bcc8f8
Ragaa Aly <ragaa.aly20@gmail.com>
2022-09-05 13:18:18 +0200

    sql file updated

--- AdminAddCourse.sql
+++ AdminAddCourse.sql
@@ -1,1 +1,13 @@
-﻿
+﻿-- Major table data
+
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'csen', N'Media Engoneering and Technology')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'dmet', N'Media Engoneering and Technology')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'networks', N'Information Engineering and Technology')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'communications', N'Information Engineering and Technology')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'electronics', N'Information Engineering and Technology')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'bi', N'Management Technology')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'management', N'Management Technology')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'pharmacy', N'Pharmacy and Biotechnology')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'ems', N'Engineering and Materials Science')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'mctr', N'Engineering and Materials Science')
+INSERT INTO [dbo].[Major] ([name], [faculty]) VALUES (N'graduate', N'Postgraduate Studies')
