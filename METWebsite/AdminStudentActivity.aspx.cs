3ee112486e879f0991ebdaf4a46ddfec08a28165
ZiadHegazy <ziadayman9901@gmail.com>
2022-09-08 02:25:35 +0200

    updated admin student activity

--- METWebsite/AdminStudentActivity.aspx.cs
+++ METWebsite/AdminStudentActivity.aspx.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -14,13 +15,19 @@ namespace METWebsite
    
     public partial class AdminStudentActivity : System.Web.UI.Page
     {
+        public static int count = 0;
+        public static ArrayList boxes=new ArrayList();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            for(int i = 0; i < boxes.Count; i++)
+            {
+                hidden.Controls.Add((Control)boxes.ToArray().ElementAt(i));
+            }
         }
 
         protected void addStudentActivity(object sender, EventArgs e)
         {
+           
             string strcon = System.Configuration.ConfigurationManager.ConnectionStrings["MET"].ConnectionString;
             //create new sqlconnection and connection to database by using connection string from web.config file  
             SqlConnection con = new SqlConnection(strcon);
@@ -39,47 +46,71 @@ namespace METWebsite
             cmd.Parameters.Add(new SqlParameter("@about", aboutClub));
             cmd.Parameters.Add(new SqlParameter("@facebook", facebookLinkClub));
             cmd.Parameters.Add(new SqlParameter("@instagram", instagramLinkClub));
+            
 
-            if (color1.Style["width"] == "25px"){
+            if (color1.Attributes["ok"]!="" || color1.Style["width"] == "25px"){
                 cmd.Parameters.Add(new SqlParameter("@gradient", "linear-gradient(to right,rgba(0, 0, 0, 1),rgba(237, 28, 36, 0.9))"));
             }
-            else if (color2.Style["width"] == "25px")
+            else if (color2.Attributes["ok"] != "" || color2.Style["width"] == "25px")
             {
                 cmd.Parameters.Add(new SqlParameter("@gradient", "linear-gradient(to left,rgba(22, 160, 93, 1),rgba(79, 141, 245, 1),rgba(22, 160, 93, 1))"));
             }
-            else if (color3.Style["width"] == "25px")
+            else if (color3.Attributes["ok"] != "" || color3.Style["width"] == "25px")
             {
                 cmd.Parameters.Add(new SqlParameter("@gradient", "linear-gradient(to left,rgba(10, 71, 152, 0.5),rgba(10, 71, 152, 1))"));
             }
-            else if (color4.Style["width"] == "25px")
+            else if (color4.Attributes["ok"] != "" || color4.Style["width"] == "25px")
             {
                 cmd.Parameters.Add(new SqlParameter("@gradient", "linear-gradient(to right,RGBA(27,51,124,100),RGBA(237,28,36,90))"));
             }
-            else if (color5.Style["width"] == "25px")
+            else if (color5.Attributes["ok"] != "" || color5.Style["width"] == "25px")
             {
                 cmd.Parameters.Add(new SqlParameter("@gradient", "linear-gradient(to right,rgba(51, 102, 187, 1),rgba(51, 102, 187, 1),rgba(37, 175, 68, 1))"));
             }
+            
             cmd.ExecuteNonQuery();
-            con.Close();
-
-
-            con.Open();
-            SqlCommand cmd2 = new SqlCommand("addTrackToStudentActivity", con);
-            cmd2.CommandType = System.Data.CommandType.StoredProcedure;
-
-            foreach (HtmlGenericControl c in hidden.Controls)
-            {
-
-                if (c.InnerHtml != null)
-                {
-                    Response.Write(c.InnerHtml);
-                    Response.Write(c.Attributes["value"]);
-                    cmd2.Parameters.Add(new SqlParameter("@track", c.InnerHtml));
-                }
-            }
-            cmd2.ExecuteNonQuery();
+            
             con.Close();
             
+
+
+            
+
+            //foreach (TextBox c in hidden.Controls)
+            //{
+            //    Response.Write(c.Text);
+                
+            //    SqlCommand cmd2 = new SqlCommand("addTrackToStudentActivity", con);
+            //    cmd2.CommandType = System.Data.CommandType.StoredProcedure;
+            //    if (c.Text !="" )
+            //    {
+            //        con.Open();
+            //        cmd2.Parameters.Add(new SqlParameter("@track", c.Text));
+            //        cmd2.ExecuteNonQuery();
+            //        con.Close();
+            //    }
+            //}
+
+            
+        }
+
+        protected void Unnamed2_Click(object sender, EventArgs e)
+        {
+            
+                TextBox t1 = new TextBox();
+                
+                t1.Attributes.Add("type", "text");
+                t1.Attributes.Add("class", "input");
+
+                
+
+
+                t1.Attributes.Add("runat", "server");
+              boxes.Add(t1);
+               hidden.Controls.Add(t1);
+           
+            
+
         }
     }
 
